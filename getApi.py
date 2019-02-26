e3be73ddd25f1187d35ddf68dc48631464a6572e
kioyoja07 <kiyoja07@gmail.com>
2019-02-26 22:22:27 +0900

    get details of seq on list

--- getApi.py
+++ getApi.py
@@ -5,20 +5,56 @@
 import urllib.request
 import config
 
-API_URL = 'http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getBassInfoSearch'
-API_KEY = config.apiKey()
+API_URL_BASE = 'http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/'
+service_key = config.apiKey()
 
 
-def preQuerySender(province, city, dong, companyNum):
-    url = API_URL
-    service_key = API_KEY
+def getSeq(province, city, dong, companyNum):
+
+    url_getSeq = "getBassInfoSearch"
+    url = API_URL_BASE + url_getSeq
     call_info = '&ldong_addr_mgpl_dg_cd=' + province + '&ldong_addr_mgpl_sggu_cd=' + city + '&ldong_addr_mgpl_sggu_emd_cd=' + dong + '&bzowr_rgst_no=' + companyNum
 
     request = urllib.request.Request(url + '?serviceKey=' + service_key + call_info)
     request.get_method = lambda: 'GET'
     response_body = urllib.request.urlopen(request).read()
 
-    preResponseResult = str(response_body, "utf-8")
-    return preResponseResult
+    response_result = str(response_body, "utf-8")
+    return response_result
 
-print(preQuerySender('41', '117', '101', '124815'))
+def getDetails(seq):
+
+    url_getDetails = "getDetailInfoSearch"
+
+    url = API_URL_BASE + url_getDetails
+    call_info = '&seq=' + str(seq) # + '&data_crt_ym=' + str(year_month)
+
+    request = urllib.request.Request(url + '?serviceKey=' + service_key + call_info)
+    request.get_method = lambda: 'GET'
+    response_body = urllib.request.urlopen(request).read()
+
+    response_result = str(response_body, "utf-8")
+    return response_result
+
+
+
+
+
+
+# print(getDetails(62785249))
+
+# def getPeriod(seq):
+
+#     url_getPeriod = "getPdAcctoSttusInfoSearch"
+
+#     url = API_URL_BASE + url_getPeriod
+#     call_info = '&seq=' + str(seq) # + '&data_crt_ym=' + str(year_month)
+
+#     request = urllib.request.Request(url + '?serviceKey=' + service_key + call_info)
+#     request.get_method = lambda: 'GET'
+#     response_body = urllib.request.urlopen(request).read()
+
+#     preResponseResult = str(response_body, "utf-8")
+#     return preResponseResult
+
+# print(getPeriod(62785249))
